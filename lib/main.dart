ca6fc898db22a0a530b706618b60a03b35d9af8e
FaisalOktabrianSholihin <faisalokta08@gmail.com>
2022-11-12 22:51:20 +0700

    minggu-10

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Get Api',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
       ),
       home: GetDataScreen(),
     );
